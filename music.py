7683dedd772b34f13a0b51cede44d4812bf9011d
Lukian <lukian55d@gmail.com>
2025-07-22 22:54:27 +0100

    🛠️ Add token protection

--- music.py
+++ music.py
@@ -4,7 +4,7 @@ import telebot
 from telebot import types
 from yt_dlp import YoutubeDL
 
-Bot = telebot.TeleBot("7949282737:AAE6nVeoNoL-nMdF_U0h3itKKV1FClIoRBU")
+Bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
 # 🔧 Словник для збереження результатів пошуку по chat_id
 track_results = {}
